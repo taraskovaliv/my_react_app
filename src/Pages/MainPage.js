9543180cfc712c197f7e5c4a5f771abf2650ba40
tkov1 <taraskovaliv@outlook.com>
2020-10-08 05:24:45 +0300

    Add news component with css
    Edit MainPage and Header.css
    Add new image

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -2,15 +2,24 @@ import React from 'react';
 import Header from "../Header/Header";
 import Menu from "./Menu";
 import Footer from "../Footer/Footer";
+import News from "./News/News";
 
-function MainPage() {
-    return (
-        <div>
-            <Header/>
-            <Menu/>
-            <Footer/>
-        </div>
-    );
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Menu/>
+                <News
+                    q={
+                        "https://newsapi.org/v2/everything?q=" +
+                        "dogs" +
+                        "&apiKey=b79c8842b7ae4158ae4f5f7fc0c48312&pageSize=25&sortBy=relevancy"
+                    }/>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default MainPage;
---
+++ src/Pages/News/News.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './News.css'
+
+class News extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(this.props.q)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    this.setState({
+                        isLoaded: true,
+                        news: result.articles
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const {error, isLoaded, news} = this.state;
+        if (error) {
+            return <div>Помилка: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Завантаження...</div>;
+        } else {
+            return (
+                <div className="News">
+                    <ul>
+                        {
+                            news.map((news) =>
+                                <li>
+                                    <div className="NewsLeft">
+                                        <img className="NewsImage"
+                                             src={news.urlToImage != null ? news.urlToImage : 'images/noimage.png'}
+                                             alt={news.title}/>
+                                    </div>
+                                    <div className="NewsRight">
+                                        <h2>{news.title}</h2>
+                                        <label>Author: <i>{news.author}</i></label>
+                                        <p>{news.description}</p>
+                                        <a href={news.url} target="_blank">
+                                            <button>Читати</button>
+                                        </a>
+                                    </div>
+                                </li>
+                            )
+                        }
+                    </ul>
+                </div>
+            );
+        }
+    }
+}
+
+export default News;
